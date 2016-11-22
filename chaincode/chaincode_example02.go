3602c1ff3a37415250202734be579159bcd0173d
aaronali <aaronali780@gmail.com>
2016-11-22 13:58:46 -0500

    update

--- chaincode/chaincode_example02.go
+++ chaincode/chaincode_example02.go
@@ -31,7 +31,7 @@ import (
 type SimpleChaincode struct {
 }
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterfaceInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Printf("Init called, initializing chaincode")
 	
 	var A, B string    // Entities
